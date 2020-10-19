fe0206bab40e82b995f86a3e63f058419cf52961
Piotr Tutak <p.tutak@outlook.com>
2020-10-20 00:42:20 +0200

    layers

---
+++ load_layers.m
@@ -0,0 +1,75 @@
+function [layers] = load_layers()
+    layers = [
+        imageInputLayer([256 256 3])
+
+        convolution2dLayer(3,64,'Padding','same')
+        batchNormalizationLayer
+        reluLayer
+
+        convolution2dLayer(3,64,'Padding','same')
+        batchNormalizationLayer
+        reluLayer
+
+        maxPooling2dLayer(2,'Stride',2)
+
+        convolution2dLayer(3,128,'Padding','same')
+        batchNormalizationLayer
+        reluLayer
+        
+        convolution2dLayer(3,128,'Padding','same')
+        batchNormalizationLayer
+        reluLayer
+
+        maxPooling2dLayer(2,'Stride',2)
+
+        convolution2dLayer(3,256,'Padding','same')
+        batchNormalizationLayer
+        reluLayer
+        
+        convolution2dLayer(3,256,'Padding','same')
+        batchNormalizationLayer
+        reluLayer
+        
+        convolution2dLayer(3,256,'Padding','same')
+        batchNormalizationLayer
+        reluLayer
+
+        maxPooling2dLayer(2,'Stride',2)
+
+        convolution2dLayer(3,512,'Padding','same')
+        batchNormalizationLayer
+        reluLayer
+
+        convolution2dLayer(3,512,'Padding','same')
+        batchNormalizationLayer
+        reluLayer
+
+        convolution2dLayer(3,512,'Padding','same')
+        batchNormalizationLayer
+        reluLayer
+
+        maxPooling2dLayer(2,'Stride',2)
+
+        convolution2dLayer(3,512,'Padding','same')
+        batchNormalizationLayer
+        reluLayer
+
+        convolution2dLayer(3,512,'Padding','same')
+        batchNormalizationLayer
+        reluLayer
+
+        convolution2dLayer(3,512,'Padding','same')
+        batchNormalizationLayer
+        reluLayer
+
+        maxPooling2dLayer(2,'Stride',2)
+
+        fullyConnectedLayer(4096)
+        fullyConnectedLayer(4096)
+        fullyConnectedLayer(10)
+
+        softmaxLayer
+        classificationLayer
+    ];
+end
+
