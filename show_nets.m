be7f95aef95c5f8629d0555b6e33c3884ceb7c9d
Piotr Tutak <p.tutak@outlook.com>
2021-01-13 23:59:31 +0100

    statistics

--- show_nets.m
+++ show_nets.m
@@ -15,19 +15,27 @@ for i=1:length(files)
 end
 
 networks_table = table;
-
+layer_tables = {};
 for i = 1:length(networks)
     network = networks{i};
     new_table = struct2table(network.options);
     accuracy = network.accuracy;
     accuracy_table = table(accuracy);
-    name = network.name;
+    name = string(['network_', num2str(i)]);
     name_table = table(name);
     new_table = [name_table, new_table, accuracy_table];
     networks_table = [networks_table; new_table];
+    layer_table = table(network.layers, 'VariableNames', name);
+    layer_tables{i} = layer_table;
 end
 
 % Modyfikacje obrazu: "RandXScale: [0.9000 1.1000], RandYScale: [0.9000 1.1000], RandXTranslation: [-30 30], RandYTranslation: [-30 30]"
 
-new_figure = uifigure;
-uitable(new_figure, "Data", networks_table, "FontSize", 18);
+networks_table;
+layer_tables;
+
+writetable(networks_table, "statistics.xlsx", 'Sheet', 'General');
+for i = 1:length(layer_tables)
+    range = [char(i + 64), '1'];
+    writetable(layer_tables{i}, "statistics.xlsx", 'Sheet', 'Layers', 'Range', range);
+end
