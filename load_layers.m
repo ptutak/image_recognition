777767e61ddfda5328c50a286fb2c27a06a7b5c3
Piotr Tutak <p.tutak@outlook.com>
2021-01-14 21:14:11 +0100

    changes

--- load_layers.m
+++ load_layers.m
@@ -2,45 +2,47 @@ function [layers] = load_layers(input_size)
     layers = [
         imageInputLayer(input_size)
         
-        convolution2dLayer(3,64,'Padding','same')
+        convolution2dLayer(3,32,'Padding','same')
         batchNormalizationLayer
         reluLayer
 
-        convolution2dLayer(3,64,'Padding','same')
+        convolution2dLayer(3,32,'Padding','same')
         batchNormalizationLayer
         reluLayer
 
-        convolution2dLayer(1,64,'Padding','same')
-        batchNormalizationLayer
-        reluLayer
         
         maxPooling2dLayer(2,'Stride',2)
 
-        convolution2dLayer(3,64,'Padding','same')
+        convolution2dLayer(3,32,'Padding','same')
         batchNormalizationLayer
         reluLayer
         
-        convolution2dLayer(3,64,'Padding','same')
+        convolution2dLayer(3,32,'Padding','same')
         batchNormalizationLayer
         reluLayer
  
-        convolution2dLayer(1,64,'Padding','same')
+
+        maxPooling2dLayer(2,'Stride',2)
+
+        convolution2dLayer(3, 64,'Padding','same')
         batchNormalizationLayer
         reluLayer
+        
+        convolution2dLayer(3,64,'Padding','same')
+        batchNormalizationLayer
+        reluLayer
+        
 
         maxPooling2dLayer(2,'Stride',2)
 
         convolution2dLayer(3,128,'Padding','same')
         batchNormalizationLayer
         reluLayer
-        
+
         convolution2dLayer(3,128,'Padding','same')
         batchNormalizationLayer
         reluLayer
-        
-        convolution2dLayer(1,128,'Padding','same')
-        batchNormalizationLayer
-        reluLayer
+
 
         maxPooling2dLayer(2,'Stride',2)
 
@@ -52,32 +54,14 @@ function [layers] = load_layers(input_size)
         batchNormalizationLayer
         reluLayer
 
-        convolution2dLayer(1,256,'Padding','same')
-        batchNormalizationLayer
-        reluLayer
-
         maxPooling2dLayer(2,'Stride',2)
 
-        convolution2dLayer(3,512,'Padding','same')
-        batchNormalizationLayer
-        reluLayer
-
-        convolution2dLayer(3,512,'Padding','same')
-        batchNormalizationLayer
-        reluLayer
-
-        convolution2dLayer(1,512,'Padding','same')
-        batchNormalizationLayer
-        reluLayer
-
-        maxPooling2dLayer(2,'Stride',2)
-
-        fullyConnectedLayer(768)
+        fullyConnectedLayer(512)
         reluLayer
 
         dropoutLayer(0.35)
 
-        fullyConnectedLayer(768)
+        fullyConnectedLayer(512)
         reluLayer
     
         fullyConnectedLayer(10)
